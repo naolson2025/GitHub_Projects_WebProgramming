6a0ac2aa33231fa91d0321f0f60642e025e2bc4d
NA <NA>
2019-02-10 16:01:55 -0600

    Added longest bridges styles and scripts

---
+++ Lab_4/longest_bridges_map/longest_bridges_script.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+//Adding bridge locations
+let verrazano_Narrows_Bridge = [40.6066, -74.0447];
+let verrazano_marker = L.marker(verrazano_Narrows_Bridge)
+    .bindPopup("Verrazano-Narrows<br>")
+    .addTo(map);
+
+
+let golden_gate_bridge = [37.8199, -122.4783];
+
+
+
+let mackinac_bridge = [45.8174, -84.7278];
+
+
+
+let george_washington_bridge = [40.8517, -73.9527];
+
+
+
+let tacoma_narrows_bridge = [47.2690, -122.5517];
+
