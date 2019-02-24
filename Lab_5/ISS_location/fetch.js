51d4b4b3e3a0d303a05bbd7d8d2889fe35f300bf
NA <NA>
2019-02-24 12:35:47 -0600

    attempted to update ISS location

--- Lab_5/ISS_location/fetch.js
+++ Lab_5/ISS_location/fetch.js
@@ -1,11 +1,12 @@
-let url = 'https://api.wheretheiss.at/v1/satellites/25544';
+let url = 'http://api.open-notify.org/iss-now.json';
 
 let issLat = document.querySelector('#iss-lat');
 let issLong = document.querySelector('#iss-long');
 let time = document.querySelector('#time');
 
-let issMarker;  // Leaflet marker
-let update = 10000;  // 10 seconds
+let issMarker;
+// Update every 10 sec
+let update = 10000;
 
 let map = L.map('iss-map').setView([0, 0], 1);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -22,41 +23,36 @@ let icon = L.icon({
 });
 
 let max_failed_attempts = 3;
-iss(max_failed_attempts);   // initial call to function. Once the fetch request has been made, the iss function
-// will call itself again, after a delay of update miliseconds, if the max number of failed attempts is not exceeded.
+iss(max_failed_attempts);
 
 function iss(attempts) {
     if ( attempts <= 0 ) {
-        console.log('Too many errors, abandoning requests to get ISS position.')
-        return;    // Since setTimeout is not called again, no more attempts to fetch will be made
+        console.log('Too many errors, abandoning requests to get ISS position.');
+        return;
     }
 
     fetch(url)
         .then( res => res.json() )
-        .then( issData => {
-            console.log(issData);
-            let lat = issData.latitude;
-            let long = issData.longitude;
+        .then( iss_position => {
+            console.log(iss_position);
+            let lat = iss_position.latitude;
+            let long = iss_position.longitude;
             issLat.innerHTML = lat;
             issLong.innerHTML = long;
 
             if (!issMarker) {
-                issMarker = L.marker([lat, long], {icon: icon}).addTo(map); // Create the marker
+                issMarker = L.marker([lat, long], {icon: icon}).addTo(map);
             } else {
-                issMarker.setLatLng([lat, long]); // Already exists - move to new location
+                issMarker.setLatLng([lat, long]);
             }
 
-            // Update the time element to the current date and time
             let date = Date();
             time.innerHTML = date
-
         })
         .catch( err => {
             console.log(err)
         })
         .finally( () => {
-        // finally runs whether the fetch() worked or failed.
-        // Call the iss function after a delay of update miliseconds to update the position
         setTimeout(iss, update, attempts)
         })
 }
