3590a0008b7ab9de59a669e0eeda0024ab65a45c
NA <NA>
2019-04-01 19:16:48 -0500

    finished slides

--- student-sign-in/src/App.vue
+++ student-sign-in/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <NewStudentForm v-on:student-added="newStudentAdded"></NewStudentForm>
-    <StudentTable v-bind:students="students" v-on:student-present="studentArrivedOrLeft"></StudentTable>
+    <StudentTable v-bind:students="students"
+                  v-on:student-present="studentArrivedOrLeft"
+                  v-on:delete-student="studentDeleted"></StudentTable>
     <StudentMessage v-bind:message="message" v-bind:name="name"></StudentMessage>
   </div>
 </template>
@@ -29,12 +31,17 @@ export default {
     newStudentAdded(student){
       this.students.push(student);
       this.students.sort(function (s1, s2) {
-        return s1.name.toLowerCase() < s2.name.toUpperCase() ? -1 : 1
+        return s1.name.toLowerCase() > s2.name.toUpperCase() ? -1 : 1
       })
     },
     studentArrivedOrLeft(student){
       this.message = student.present ? 'Welcome, ' : 'Goodbye, ';
       this.name = student.name
+    },
+    studentDeleted(student){
+      this.students = this.students.filter( function (s) {
+        return s !== student
+      })
     }
   }
 }
--- student-sign-in/src/components/StudentTable.vue
+++ student-sign-in/src/components/StudentTable.vue
@@ -3,20 +3,28 @@
         <!-- Display section for students -->
         <div class="card student-list m-2 p-2">
             <h4 class="card-title">Student</h4>
+
+            <div class="edit-table-toggle form-check">
+                <input id="edit-table" type="checkbox" class="form-check-input" v-model="editTable">
+                <label for="edit-table" class="form-check-label">Edit table?</label>
+            </div>
+
             <div id="student-table">
                 <table class="table">
                     <tr>
                         <th>Name</th>
                         <th>StarID</th>
                         <th>Present?</th>
+                        <th v-show="editTable">Delete</th>
                     </tr>
 
-                    <!--use a for loop to add every student in the students data list to a table row-->
-                    <tr v-for="student in students" v-bind:class="'present-' + student.present">
-                        <td>{{ student.name }}</td>
-                        <td>{{ student.starID }}</td>
-                        <td> <input type="checkbox" v-model="student.present" v-on:change="checked(student)"> </td>
-                    </tr>
+                    <StudentRow
+                        v-for="student in students" v-bind:key="student.name"
+                        v-bind:student="student"
+                        v-bind:edit="editTable"
+                        v-on:student-present="studentArrivedOrLeft"
+                        v-on:delete-student="studentDeleted">
+                    </StudentRow>
                 </table>
             </div>
         </div>
@@ -24,14 +32,24 @@
 </template>
 
 <script>
+    import StudentRow from "./StudentRow";
     export default {
         name: "StudentTable",
+        components: {StudentRow},
+        data(){
+            return {
+                editTable: false
+            }
+        },
         props: {
             students: Array
         },
         methods: {
-            checked(student){
+            studentArrivedOrLeft(student){
                 this.$emit('student-present', student)
+            },
+            studentDeleted(student){
+                this.$emit('delete-student', student)
             }
         }
     }
