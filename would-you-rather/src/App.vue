bc41129b47c2eee343bd71410a3118d6a11e8a56
NA <NA>
2019-04-02 19:29:36 -0500

    working lab 8 would you rather vue-cli

---
+++ would-you-rather/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div v-for="question in questions">
+      <li>
+        <WouldYouRatherQuestion
+                v-bind:question="question.question"
+                v-bind:answer1="question.answer1"
+                v-bind:answer2="question.answer2"
+                v-on:answer-changed="answerChanged"
+        ></WouldYouRatherQuestion>
+      </li>
+    </div>
+  </div>
+</template>
+
+<script>
+import WouldYouRatherQuestion from './components/WouldYouRatherQuestion'
+
+export default {
+  name: 'app',
+  components: {
+    WouldYouRatherQuestion
+  },
+  data(){
+    return{
+      questions: [
+        {
+          id: 0,
+          question: 'be a wizard or a superhero?',
+          answer1: 'Be a wizard',
+          answer2: 'Be a superhero'
+        },
+        {
+          id: 1,
+          question: 'be able to type/text very fast or be able to read really quickly?',
+          answer1: 'Type fast',
+          answer2: 'Read fast'
+        },
+        {
+          id: 2,
+          question: 'be bulletproof or be able to survive falls from any height?',
+          answer1: 'Be bulletproof',
+          answer2: 'Survive falls from any height'
+        }
+      ]
+    }
+  },
+  methods: {
+    answerChanged(answer){
+      this.userSelectionMessage = `Thanks! You selected ${answer}`
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ would-you-rather/src/components/WouldYouRatherQuestion.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>{{ question }}</h2>
+        <input type="radio" v-model="choice" v-bind:value="question.answer1" v-on:change="$emit('answer-changed', choice)">
+        <label>{{ answer1 }}</label>
+        <input type="radio" v-model="choice" v-bind:value="question.answer2" v-on:change="$emit('answer-changed', choice)">
+        <label>{{ answer2 }}</label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "WouldYouRatherQuestion",
+        data() {
+            return {
+                choice: ''
+            }
+        },
+        props: {
+            question: String,
+            answer1: String,
+            answer2: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
